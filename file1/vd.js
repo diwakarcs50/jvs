44fb225a625414a017ccc03723f636331d752ad8
diwakarcs50 <85155726+diwakarcs50@users.noreply.github.com>
2023-08-14 10:04:04 +0000

    this is variable and data types

---
+++ file1/vd.js
@@ -0,0 +1,18 @@
+// "use strict";
+// const statename="bihar"
+// let scity="ajsj"
+// var sday=978
+// sdays="tuesday"
+// console.table([statename,scity,sday,sdays])
+
+
+//variables
+
+let h1=90
+let h2="test"
+let h3;
+//null =>data type of null is object ???
+//object=>it is an important concept to learn
+//symbol =>uniqueness (this is used mostly in React )
+
+console.table([typeof h1,typeof h2,typeof null,typeof symbol,typeof h3,typeof NaN])
