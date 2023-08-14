d68202ae0cb0f1a5e671844dcdea04b33200768b
diwakarcs50 <85155726+diwakarcs50@users.noreply.github.com>
2023-08-14 21:18:42 +0000

    numbers & maths

---
+++ file1/math.js
@@ -0,0 +1,20 @@
+const num=534
+const num1=new Number(679)
+// console.log(num1)
+
+const val=num.toString()
+// console.log(typeof val)
+
+let num2=1000000
+// console.log(num2.toLocaleString())
+// console.log(num2.toLocaleString('en-IN'))
+// console.log(num.toFixed(2))
+
+
+console.log(Math.random())
+console.log(Math.floor((Math.random()*10)+1))
+//if we want a number a maximum and minimum range
+const max=10
+const min=20
+//we have a defined formula
+console.log(Math.floor(Math.random()*((max-min)+1)+min))
