620e36fde97f615b26003a77097ab99690f6a4a0
diwakarcs50 <85155726+diwakarcs50@users.noreply.github.com>
2023-08-15 22:16:54 +0000

    Destructuring

--- file1/objects.js
+++ file1/objects.js
@@ -33,25 +33,49 @@
 
 
 //object version 2
-const obj=new Object()
-console.log(obj)
-obj.user="diwakar"
-obj.name="anyname"
-console.log(obj)
+// const obj=new Object()
+// console.log(obj)
+// obj.user="diwakar"
+// obj.name="anyname"
+// console.log(obj)
 
-const obj1={1:'a',2:'b'}
-const obj2={3:'c',4:'d'}
+// const obj1={1:'a',2:'b'}
+// const obj2={3:'c',4:'d'}
 // const obj3={obj1,obj2}
 // const obj3={...obj1,...obj2}
-const obj3=Object.assign({},obj1,obj2)//we take it as Object.assign(target,source)
-console.log(obj3)
-console.log(Object.keys(obj3))
-console.log(Object.values(obj3))
-//we can have nested objects
+// const obj3=Object.assign({},obj1,obj2)//we take it as Object.assign(target,source)
+// console.log(obj3)
+// console.log(Object.keys(obj3))
+// console.log(Object.values(obj3))
+// //we can have nested objects
 
-console.log(Object.entries(obj3))//It will change every object in arrays
+// console.log(Object.entries(obj3))//It will change every object in arrays
 //we can always use chrome console
 
+//destructuring in object
+
+const obj1={
+    coursename:"js in hindi",
+    counseInstructor:"hc",
+    price:999
+}
+// console.log(obj1.coursename)
+
+const {coursename : newname}=obj1//here we can destructure and even provide a newname
+console.log(newname)
+// console.log(newname)
+
+// this is a json format
+// {
+//     "name":"diwkar",
+//     "age":"22"
+// }
+//data can also arrive in this format
+// [
+//     {}
+//     {}
+
+// ]
 
 
 
