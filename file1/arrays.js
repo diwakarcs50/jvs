39d88602355f84af43a73403ec96c5af61b844f0
diwakarcs50 <85155726+diwakarcs50@users.noreply.github.com>
2023-08-15 15:11:45 +0000

    second part array

--- file1/arrays.js
+++ file1/arrays.js
@@ -1,34 +1,56 @@
 //arrays can be declared in two ways
-const mixarr=[1,2,3,4,"ytsk",'u']
-console.log(mixarr)
-const myarr=[1,2,3,4,5]
-console.log(myarr)
-const newarr=myarr.join() //converts arrays to strings
-console.log(newarr)
+// const mixarr=[1,2,3,4,"ytsk",'u']
+// console.log(mixarr)
+// const myarr=[1,2,3,4,5]
+// console.log(myarr)
+// const newarr=myarr.join() //converts arrays to strings
+// console.log(newarr)
 
-myarr.push(6);
-console.log(myarr)
+// myarr.push(6);
+// console.log(myarr)
 
-myarr.pop()
-console.log(myarr)
+// myarr.pop()
+// console.log(myarr)
 
-const myarr1=new Array(1,2,3,4,53)
-console.log(myarr1)
+// const myarr1=new Array(1,2,3,4,53)
+// console.log(myarr1)
 
-myarr1.unshift(45)//adds the elment at the front
-console.log(myarr1)
-myarr1.unshift("dk")
-console.log(myarr1)
+// myarr1.unshift(45)//adds the elment at the front
+// console.log(myarr1)
+// myarr1.unshift("dk")
+// console.log(myarr1)
 
-console.log(myarr1.shift())//note:when we are printing it is giving the first element
-console.log(myarr1)
+// console.log(myarr1.shift())//note:when we are printing it is giving the first element
+// console.log(myarr1)
 
-//slice and splice are important
+// //slice and splice are important
 
-console.log("A ",myarr)
-console.log(myarr.slice(1,3))
-console.log(myarr)//myarr is not changed 
+// console.log("A ",myarr)
+// console.log(myarr.slice(1,3))
+// console.log(myarr)//myarr is not changed 
 
-console.log(myarr.splice(1,3))
-console.log("B ",myarr)//myarr is changed
+// console.log(myarr.splice(1,3))
+// console.log("B ",myarr)//myarr is changed
+
+//array part 2
+
+//push
+const myarr=[1,2,3,4,5,6]
+const myarr1=[3,5,7,9]
+// myarr.push(myarr1)
+// console.log(myarr)
+//concat
+// const newarr=myarr.concat(myarr1)
+// console.log(newarr)
+
+// const anotherarray=[1,2,3,4,[4,5,6,7,[4]]]
+// const otherarray=anotherarray.flat(Infinity)
+// console.log(otherarray)
+
+
+//split operator 
+
+console.log(...myarr,...myarr1)
+
+
 
