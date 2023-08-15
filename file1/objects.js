8d5188ff9c5707ce3fc48bdaa119dfc0d79d1856
diwakarcs50 <85155726+diwakarcs50@users.noreply.github.com>
2023-08-15 20:35:55 +0000

    objects version2

--- file1/objects.js
+++ file1/objects.js
@@ -3,13 +3,13 @@
 //Object.create(())
 
 //onject literals
-const asym=Symbol("india")
-const juser={
-    name:"diwakar",
-    email:"diwakar@google.com",
-    asym:"country",
-    ["sym"]:"unique"
-}
+// const asym=Symbol("india")
+// const juser={
+//     name:"diwakar",
+//     email:"diwakar@google.com",
+//     asym:"country",
+//     ["sym"]:"unique"
+// }
 // console.log(juser);
 // console.log(juser.name)
 // console.log(juser["name"])
@@ -20,16 +20,38 @@ const juser={
 // console.log(juser)
 // console.log(juser.sym);
 
-juser.fun=function(){
-    // console.log("he is a jsuser")
-}
-juser.fun2=function(){
-    console.log(`this object name is ${this.name}`)
-}
-juser.fun3=function(){
-    console.log(`this object email is ${this.email}`);
-}
-console.log(juser.fun3())
+// juser.fun=function(){
+//     // console.log("he is a jsuser")
+// }
+// juser.fun2=function(){
+//     console.log(`this object name is ${this.name}`)
+// }
+// juser.fun3=function(){
+//     console.log(`this object email is ${this.email}`);
+// }
+// console.log(juser.fun3())
+
+
+//object version 2
+const obj=new Object()
+console.log(obj)
+obj.user="diwakar"
+obj.name="anyname"
+console.log(obj)
+
+const obj1={1:'a',2:'b'}
+const obj2={3:'c',4:'d'}
+// const obj3={obj1,obj2}
+// const obj3={...obj1,...obj2}
+const obj3=Object.assign({},obj1,obj2)//we take it as Object.assign(target,source)
+console.log(obj3)
+console.log(Object.keys(obj3))
+console.log(Object.values(obj3))
+//we can have nested objects
+
+console.log(Object.entries(obj3))//It will change every object in arrays
+//we can always use chrome console
+
 
 
 
