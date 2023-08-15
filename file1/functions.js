7c30495ca1c67227340763063ede3a92f689fa89
diwakarcs50 <85155726+diwakarcs50@users.noreply.github.com>
2023-08-15 23:08:59 +0000

    functions

---
+++ file1/functions.js
@@ -0,0 +1,36 @@
+//basics of functions in javascript
+// function logUser(username){
+//     console.log(`${username} logged in`)
+// }
+
+// function logUser(username="default"){//default signifies when we do not pass any value it chooses default
+//     if(!username){
+//         console.log("enter a valid username")
+//          return
+//     }
+//     // if(username==undefined){
+//     //     console.log("enter a valid username")
+//     //     return
+//     // }
+//     console.log(`${username} logged in`)
+// }
+// logUser()
+
+// function calculatecart(...num1){//here we dont need to define how many parameters are going to come
+//     console.log(num1)
+// }
+// console.log(calculatecart(10,20,30,40));
+
+//passing of objects 
+//similarly we can pass arrays
+const obj1={
+    name:"username",
+    id:"userid"
+}
+
+const {name:n,id:i}=obj1
+function user(arrive){
+    console.log(`${n} is the user with is ${i}`)
+
+}
+user(obj1)
